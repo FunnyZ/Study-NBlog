2e88e29a30443a37166c7bbb378e2e2316d6b145
zjh <zjh@zjhdeMacBook-Pro.local>
2017-03-06 23:56:49 +0800

    basis files

---
+++ middlewares/check.js
@@ -0,0 +1,19 @@
+module.exports = {
+    checkLogin: function checkLogin(req, res, next) {
+        if ( !req.session.user ) {
+            req.flash('error', '未登录');
+            // 跳转到登录页
+            return res.redirect('/signin');
+        }
+        next();
+    },
+
+    checkNotLogin: function checkNotLogin(req, res, next) {
+        if ( req.session.user ) {
+            req.flash('error', '已登录');
+            // 返回之前的页面
+            return res.redirect('back');
+        }
+        next();
+    }
+};
