fe542e35e080681d92189995ba002a71e2276605
zjh <zjh@zjhdeMacBook-Pro.local>
2017-03-06 23:54:14 +0800

    配置文件

---
+++ config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 监听的端口号
+    port: 3000,
+    // express-session 的配置信息
+    session: {
+        secret: 'myblog',
+        key: 'myblog',
+        maxAge: 2592000000
+    },
+    // mongodb 的地址，myblog 为 db 名
+    mongodb: 'mongodb://localhost:27017/myblog'
+};
